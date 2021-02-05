3b8ef406c36bd4f5bf568c0c7fe31bea954efd60
Patryk Stefanowicz <patrykstefanowicz95@gmail.com>
2021-02-05 17:35:22 +0100

    some editor js

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,27 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('Save - editor 🚀')
+
+
+const editor = localStorage.getItem('editor');
+let result = '';
+
+if (editor) {
+    result = editor;
+}
+const entryInput = document.querySelector('.textarea--js');
+entryInput.value = result;
+
+const buttonSave = document.querySelector('.button__save--js');
+buttonSave.addEventListener ('click' , () => {
+    localStorage.setItem ('EditorSave' , entryInput.value);
+})
+
+
+
+
+const buttonLoad = document.querySelector('.button__load--js');
+buttonSave.addEventListener ('click' , () => {
+    localStorage.setItem ('EditorLoad' , entryInput.value);
+})
